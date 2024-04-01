ae07a7b3f8fa12c8a661419903bf8572303e3a42
Brian Barasa <briannbig@gmail.com>
2024-04-01 13:37:01 +0300

    set up http routes

--- server.go
+++ server.go
@@ -1,9 +1,33 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/briannbig/habitify/handlers"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	habitHandler = handlers.NewHabitHandler()
+	userHandler  = handlers.NewUserHandler()
+)
 
 func main() {
 	r := gin.Default()
 
-	r.Run()
+	userRoutes := r.Group("/users")
+	{
+		userRoutes.POST("/", userHandler.Create)
+		userRoutes.GET("/", userHandler.GetAll)
+		userRoutes.PUT("/:id", userHandler.Update)
+		userRoutes.DELETE("/:id", userHandler.Delete)
+	}
+
+	habitRoutes := r.Group("/habits")
+	{
+		habitRoutes.POST("/", habitHandler.Create)
+		habitRoutes.GET("/", habitHandler.GetAll)
+		habitRoutes.PUT("/:id", habitHandler.Update)
+		habitRoutes.DELETE("/:id", habitHandler.Delete)
+	}
+
+	r.Run(":5050")
 }
